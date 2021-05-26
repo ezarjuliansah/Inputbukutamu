ee9dc745a2dc1d9fc467449371f4a21fe1061ae1
ezarjuliansah <80140097+ezarjuliansah@users.noreply.github.com>
2021-05-26 21:30:18 +0700

    Add files via upload

---
+++ Koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$host="localhost";
+$user_db="root";
+$pass_db="";
+$db="buku_tamu";
+$koneksi=mysqli_connect ($host,$user_db,$pass_db) or die (mysqli_error());
+mysqli_select_db ($koneksi,$db) or die (mysqli_error());
+?>
+© 2021 GitHub, Inc.
